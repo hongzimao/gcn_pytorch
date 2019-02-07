8c63ff8aeb55ff92ec2659cb4a0617c1a28d66f2
Hongzi Mao <hongzi@mit.edu>
2019-02-07 09:37:50 -0500

    add in gcn

--- gcn.py
+++ gcn.py
@@ -1,23 +1,29 @@
 # core framework follows https://github.com/tkipf/pygcn
 
+import numpy as np
+import torch
 import torch.nn as nn
+import layers
+import msg
 
 
 class GCN(nn.Module):
-    def __init__(self, n_feats, e_feats, n_output, e_output, num_hids, n_steps):
+    def __init__(self, n_feats, e_feats, n_output, e_output,
+                 h_size, n_hids, n_steps):
         '''
         n_feats: number of node features
         e_feats: number of edge features
         n_output: output size of node
         e_output: output size of edge
-        num_hids: number of hidden neurons (a list)
+        h_size: feature size in hidden space
+        n_hids: number of hidden neurons (a list)
         n_steps: number of message passing steps
 
-        There are 7 transoformations: f, g, h, k1, k2, k, l
+        There are 7 transoformations: f, g, h, k1, k2, l, q
         1. y = f(x), raises the dimension of node features
         2. y <- g(sum_neighbors y) + y, message passing
         3. z = h(y), mapping to node output
-        4. z_ij = l[k1(y_i) + k2(y_j) + k(x_ij)],
+        4. z_ij = q[k1(y_i) + k2(y_j) + l(x_ij)],
            mapping to edge output
 
         Note: if e_feats is 0, we won't have the term k(x_ij) in (4) above,
@@ -25,7 +31,79 @@ class GCN(nn.Module):
 
         Note: for simplicity, we create the same neural network
         structure (with different set of parameters) for all
-        transformation (specified by the list in num_hids).
+        transformation (specified by the list in n_hids).
         '''
 
         super(GCN, self).__init__()
+
+        self.msg = msg.MessagePassing(n_feats, n_hids, h_size, n_steps)
+        self.h = layers.Transformation(h_size, n_hids, n_output)
+        self.k1 = layers.Transformation(h_size, n_hids, h_size)
+        self.k2 = layers.Transformation(h_size, n_hids, h_size)
+        self.l = layers.Transformation(e_feats, n_hids, h_size)
+        self.q = layers.Transformation(h_size, n_hids, e_output)
+
+    def forward(self, node_feats, adj_mat, edges, edge_feats):
+        '''
+        node_feats: node features, size n * d
+        adj_mat: adjacancy matrix, size n * n
+        edges: list of edges, each edge represented by node-tuple (i, j)
+        edge_feats: edge features corresponding to the node-tuples
+        '''
+        num_nodes = node_feats.shape[0]
+
+        # message passing among nodes
+        node_embeddings = self.msg(node_feats, adj_mat)
+
+        # map to node outputs
+        node_outputs = self.h(node_embeddings)
+
+        # edge embedding
+        node_mat_1, node_mat_2 = convert_sp_node_mat(num_nodes, edges)
+        left_node_embeddings = self.k1(torch.spmm(node_mat_1, node_embeddings))
+        right_node_embeddings = self.k2(torch.spmm(node_mat_2, node_embeddings))
+        edge_embeddings = left_node_embeddings + right_node_embeddings
+        if edge_feats is not None:
+            edge_embeddings += self.l(edge_feats)
+
+        # map to edge outputs
+        edge_outputs = self.q(edge_embeddings)
+
+        return node_outputs, edge_outputs
+
+
+def convert_sp_node_mat(num_nodes, list_edges):
+    '''
+    list_edges: list of node-tuples
+
+    Example: list_edges = [(0, 2), (0, 1), (1, 2), (2, 0)]
+             num_noes = 3
+    
+    Note that there are two edges of opposite directions
+    in between node 0 and 2. 
+
+    The outputs are two sparse matrices of size
+    num_nodes * num_edges mapping nodes to end of edges.
+
+    For this example, the outputs are
+
+    [[1, 0, 0],          [[0, 0, 1],
+     [1, 0, 0],    and    [0, 1, 0],
+     [0, 1, 0],           [0, 0, 1],
+     [0, 0, 1]]           [1, 0, 0]]
+    '''
+    l = len(list_edges)
+    edge_ends = np.array(list_edges)
+    node_indices = range(l)
+    ones = torch.FloatTensor([1 for _ in range(l)])
+    mat_size = torch.Size([l, num_nodes])
+
+    node_mat_1 = torch.sparse.FloatTensor(
+        torch.LongTensor([node_indices, edge_ends[:, 0]]),
+        ones, mat_size)
+
+    node_mat_2 = torch.sparse.FloatTensor(
+        torch.LongTensor([node_indices, edge_ends[:, 1]]),
+        ones, mat_size)
+
+    return node_mat_1, node_mat_2
